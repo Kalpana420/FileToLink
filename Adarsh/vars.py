549c6deed48105b7bdc85689d3c27fab95f4fd2d
Kalpana420 <kalpanachoudharyji@gmail.com>
2024-07-31 16:54:19 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -30,11 +30,11 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
+    HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
-        URL = "https://filetolink-7gln.onrender.com".format(FQDN)
+        URL = "https://filetolink-7gln.onrender.com".(FQDN)
     else:
-        URL = "https://filetolink-7gln.onrender.com".format(FQDN)
+        URL = "https://filetolink-7gln.onrender.com".(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://u0947706:iDPVnxykM3VU3z5H@cluster0.aoy15hn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'updatechannel_tele'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
